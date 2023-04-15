1513ecbdc445bc344f21fa32cd0936d33c53c578
GraceTran <graceyen261@gmail.com>
2023-04-15 09:57:15 +0700

    first commit

---
+++ src/main/java/commons/BaseTest.java
@@ -0,0 +1,45 @@
+package commons;
+
+import java.time.Duration;
+import java.util.Random;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class BaseTest {
+protected WebDriver driver;
+
+String projectPath = System.getProperty("user.dir");
+
+protected WebDriver getBrowserDriver(String browserName){
+    switch (browserName){
+        case"firefox":
+        System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+        driver = new FirefoxDriver();
+        break;
+
+        case"chrome":
+        System.setProperty("webdriver.chrome.driver", projectPath + "\\browserDrivers\\chromedriver.exe");
+        driver = new ChromeDriver();
+        break;
+
+        case"edge":
+        System.setProperty("webdriver.edge.driver", projectPath + "\\browserDrivers\\msedgedriver.exe");
+        driver = new EdgeDriver();
+        break;
+
+        case"":
+        throw new RuntimeException("Browser is not valid");
+    }
+    driver.get("https://arvatech.sotatek.works/");
+    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    return driver;
+}
+
+    protected int fakeFandomData() {
+		Random rand = new Random();
+        return rand.nextInt();
+	}
+
+}
